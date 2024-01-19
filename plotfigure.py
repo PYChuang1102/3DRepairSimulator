f45e07669a1dd11b6d19604462c4036de669ec7a
chuang90 <po-yao.chuang@imec.be>
2024-01-19 18:02:19 +0100

    Update the functions of showing repairs

--- plotfigure.py
+++ plotfigure.py
@@ -44,123 +44,138 @@ class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.layoutCount = 3
         self.database = None
         self.dies = []
-        for i in range(self.layoutCount):
-            self.dies.append(IArray())
+        self.figures = []
+        self.figure_widget = []
 
-        self.arrows = []
-        self.routes = []
-        self.drawn_routes = []
-        self.drawnRepairGroup = []
+        self.geometry("720x360")
         self.title('3D Repair Simulator')
-        self.dpi = 60  #dpi=120
+        self.dpi = 80  #dpi=120
         self.scaler = 1.0
-        # create a figure
-        self.figure = Figure(dpi=self.dpi)
-        self.figure_die2 = Figure(dpi=self.dpi)
 
-        self.figure.suptitle('Micro-bump Map 0 (Primary)')
-        self.figure_die2.suptitle('Micro-bump Map 1 (Secondary)')
-        #self.rowconfigure(3, weight=2)
-        #self.columnconfigure(2, weight=2)
+        self.frm_buttons = tk.Frame(self, relief=tk.RAISED, bd=2)
+        self.btn_open = tk.Button(self.frm_buttons, text="Open Layout",
+                                  command=lambda: self.open_layout_file(self.dies))
+        self.btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        self.frm_buttons.pack(side=tk.LEFT, fill='y', expand=False)  # item1
 
-        frm_buttons = tk.Frame(self, relief=tk.RAISED, bd=2)
-        btn_open = tk.Button(frm_buttons, text="Open Layout", command=lambda: self.open_layout_file(self.dies))
-        btn_load_repair = tk.Button(frm_buttons, text="Load Repair", command=lambda: self.open_repair_file(self.dies))
-        btn_load_mapping = tk.Button(frm_buttons, text="Load Mapping")
-        #btn_open_die2 = tk.Button(frm_buttons, text="Open Die2", command=lambda: self.open_layout_file(self.dies[1]))
-        self.btn_stack_dies = tk.Button(frm_buttons, text="Bind Layouts", command=self.create_stacked_mapping)
-        self.btn_unstack_dies = tk.Button(frm_buttons, text="Unbind Layouts", command=self.remove_stacked_mapping)
-        #btn_open_repair_die2 = tk.Button(frm_buttons, text="Open repair die 2", command=lambda: self.open_repair_file(self.dies[1]))
-        btn_line = tk.Button(frm_buttons, text="Draw shorts", command=self.press_drawlines)
-        btn_clean_line = tk.Button(frm_buttons, text="Clean shorts", command=self.press_cleanlines)
-        btn_show_signals = tk.Button(frm_buttons, text="Show signals", command=self.show_signals)
-        #btn_clean_arrows = tk.Button(frm_buttons, text="Clean arrows", command=self.press_cleanarrows)
-        #btn_new_button = tk.Button(frm_buttons, text="New buttons", command=lambda: self.new_mode_btn(self.print_by_press, 'New'))
-        #btn_rm_mode_btn = tk.Button(frm_buttons, text="Rm buttons", command=self.remove_mode_btns)
-        btn_new_window = tk.Button(frm_buttons, text="Pop window", command=self.open_popup)
-
-
-        btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-        btn_load_repair.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-        btn_load_mapping.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-        #btn_open_repair_die2.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-        self.btn_stack_dies.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
-        btn_line.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
-        btn_clean_line.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
-        btn_show_signals.grid(row=6, column=0, sticky="ew", padx=5, pady=5)
-        #btn_clean_arrows.grid(row=7, column=0, sticky="ew", padx=5, pady=5)
-        #btn_new_button.grid(row=8, column=0, sticky="ew", padx=5, pady=5)
-        #btn_rm_mode_btn.grid(row=9, column=0, sticky="ew", padx=5, pady=5)
-        btn_new_window.grid(row=9, column=0, sticky="ew", padx=5, pady=5)
-
-        #frm_buttons.grid(row=1, column=0, sticky="ns")
-        frm_buttons.pack(side=tk.LEFT, fill='y', expand=False) #item1
+        # create textbox
+        self.frm_textbox = tk.Frame(self)
+        self.frm_textbox.pack(side=tk.BOTTOM, fill='x', expand=False)  # item4
+        self.textbox = tk.Text(self.frm_textbox, height=7)
+        self.textbox.grid(row=0, column=0, sticky="nsew")
 
         # Dynamic buttoms
         self.mode_btn = []
         self.mode_frm = None
         self.mode_frm = tk.Frame(self, relief=tk.RAISED, bd=2)
-        self.mode_frm.pack(side=tk.RIGHT, fill='y', expand=False) #item2
-
-        # create FigureCanvasTkAgg object
-        self.figure_canvas = FigureCanvasTkAgg(self.figure, self)
-        self.figure_canvas.mpl_connect('key_press_event', self.on_press)
-        self.figure_canvas.mpl_connect('button_press_event', self.mouseclick_wrapper_die1)
-
-        self.figure_die2_canvas = FigureCanvasTkAgg(self.figure_die2, self)
-        self.figure_die2_canvas.mpl_connect('button_press_event', self.mouseclick_wrapper_die2)
-
-        # create axes for die 1
-        self.dies[0].ax = self.figure.add_subplot()
-        x_left, x_right = self.dies[0].ax.get_xlim()
-        y_low, y_high = self.dies[0].ax.get_ylim()
-        self.dies[0].ax.set_aspect(abs((x_right - x_left) / (y_low - y_high)) * 1)
-
-        # create axes for die 2
-        self.dies[1].ax = self.figure_die2.add_subplot()
-        x_left, x_right = self.dies[1].ax.get_xlim()
-        y_low, y_high = self.dies[1].ax.get_ylim()
-        self.dies[1].ax.set_aspect(abs((x_right - x_left) / (y_low - y_high)) * 1)
-
-        # create textbox
-        self.frm_textbox = tk.Frame(self)
-        self.frm_textbox.pack(side=tk.BOTTOM, fill='x', expand=False) #item4
-        self.textbox = tk.Text(self.frm_textbox, height=10)
-        self.textbox.grid(row=0, column=0, sticky="nsew")
+        self.mode_frm.pack(side=tk.BOTTOM, fill='x', expand=False)  # item2
 
         # create toolbars frame
         self.frm_toolbar = tk.Frame(self)
-        self.frm_toolbar.pack(side=tk.BOTTOM, fill='x', expand=False) #item5
+        self.frm_toolbar.pack(side=tk.BOTTOM, fill='x', expand=False)  # item5
 
-            # create the toolbar for die 1
-        frm_toolbar_die1 = tk.Frame(self.frm_toolbar)
-        frm_toolbar_die1.pack(side=tk.LEFT, fill='x', expand=False)
-        toolbar = NavigationToolbar2Tk(self.figure_canvas, frm_toolbar_die1)
-        toolbar.update()
-
-            # create the toolbar for die 2
-        frm_toolbar_die2 = tk.Frame(self.frm_toolbar)
-        frm_toolbar_die2.pack(side=tk.RIGHT, fill='x', expand=False)
-        toolbar_die2 = NavigationToolbar2Tk(self.figure_die2_canvas, frm_toolbar_die2)
-        toolbar_die2.update()
-
-            # create dpi bottoms
-        btn_incr_dpi = tk.Button(self.frm_toolbar, text="+", command=lambda: self.add_dpi(10))
+        # create dpi bottoms
+        btn_incr_dpi = tk.Button(self.frm_toolbar, text="DPI+10", command=lambda: self.add_dpi(10))
         btn_incr_dpi.pack(side=tk.LEFT, fill='x', expand=False)
-        btn_decr_dpi = tk.Button(self.frm_toolbar, text="-", command=lambda: self.add_dpi(-10))
+        btn_decr_dpi = tk.Button(self.frm_toolbar, text="DPI-10", command=lambda: self.add_dpi(-10))
         btn_decr_dpi.pack(side=tk.LEFT, fill='x', expand=False)
 
-        # Plot die1 figure
-        self.figure_canvas.get_tk_widget().pack(side=tk.LEFT, fill='both', expand=True) #item6
-        # Plot die2 figure
-        self.figure_die2_canvas.get_tk_widget().pack(side=tk.LEFT, fill='both', expand=True) #item7
-
+        # create figure frame
+        self.frm_figure = tk.Frame(self)
+        self.frm_figure.pack(side=tk.TOP, fill='both', expand=True)
+        self.frm_figure.bind('<Configure>', self.resize_window)
+        # Mouse event list
+        self.mouseevent = []
     def add_dpi(self, num):
         self.dpi = self.dpi + num
         self.flush_fig()
+
+    def contruct_app(self, numDies):
+        self.dies.clear()
+        self.figures.clear()
+        self.figure_widget.clear()
+
+        self.btn_load_repair = tk.Button(self.frm_buttons, text="Load Repair",
+                                    command=lambda: self.open_repair_file(self.dies))
+        self.btn_load_mapping = tk.Button(self.frm_buttons, text="Load Mapping")
+        self.btn_stack_dies = tk.Button(self.frm_buttons, text="Bind Layouts", command=self.create_stacked_mapping)
+        self.btn_unstack_dies = tk.Button(self.frm_buttons, text="Unbind Layouts", command=self.remove_stacked_mapping)
+        self.btn_line = tk.Button(self.frm_buttons, text="Draw shorts", command=self.press_drawlines)
+        self.btn_clean_line = tk.Button(self.frm_buttons, text="Clean shorts", command=self.press_cleanlines)
+        self.btn_show_signals = tk.Button(self.frm_buttons, text="Show signals", command=self.show_signals)
+        self.btn_new_window = tk.Button(self.frm_buttons, text="Pop window", command=self.open_popup)
+        self.btn_load_repair.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_load_mapping.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_stack_dies.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_line.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_clean_line.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_show_signals.grid(row=6, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_new_window.grid(row=9, column=0, sticky="ew", padx=5, pady=5)
+
+
+        for i in range(numDies):
+            die = IArray()
+            self.dies.append(die)
+            # create a figure
+            figure = Figure(dpi=self.dpi)
+            self.figures.append(figure)
+
+            if i == 0:
+                figure.suptitle('Micro-bump Map '+str(i)+' (Primary)')
+            else:
+                figure.suptitle('Micro-bump Map ' + str(i) + ' (Secondary)')
+
+            # create mouse events
+            mouse = MouseEvent(die, func=self.mouseclicks)
+            self.mouseevent.append(mouse)
+            # create FigureCanvasTkAgg object
+            #figure_canvas = FigureCanvasTkAgg(figure, self)
+            figure_canvas = FigureCanvasTkAgg(figure, self.frm_figure)
+            figure_canvas.mpl_connect('key_press_event', self.on_press)
+            figure_canvas.mpl_connect('button_press_event', mouse.mouseclick_wrapper)
+
+            # create axes for die
+            die.ax = figure.add_subplot()
+            x_left, x_right = die.ax.get_xlim()
+            y_low, y_high = die.ax.get_ylim()
+            die.ax.set_aspect(abs((x_right - x_left) / (y_low - y_high)) * 1)
+
+            # create the toolbar for die 1
+            frm_toolbar = tk.Frame(self.frm_toolbar)
+            frm_toolbar.pack(side=tk.LEFT, fill='x', expand=False)
+            toolbar = NavigationToolbar2Tk(figure_canvas, frm_toolbar)
+            toolbar.update()
+
+            figure_widget = figure_canvas.get_tk_widget()
+            #side = tk.LEFT
+            #figure_widget.pack(side=side, fill='both', expand=True)  # item6
+            figure_widget.grid(row=int(i/2), column=i%2, sticky="nsew")
+            self.figure_widget.append(figure_widget)
+
+    def resize_window(self, event=None):
+        self.adjust_window(event.width, event.height)
+    def adjust_window(self, new_width, new_height):
+        #new_width = event.width
+        #new_height = event.height
+        #print(len(self.figure_widget))
+        num = int( (len(self.figure_widget)+1) / 2 )
+        for widget in self.figure_widget:
+            widget.configure(width=new_width/2, height=new_height/num)
+        #for fig in self.figures:
+        #    fig.set_figwidth(new_width / 2)
+        #    fig.set_figheight(new_height/2)
+
+
+    def app_clear(self):
+        del self.database
+        self.database = None
+        self.dies.clear()
+        self.figures.clear()
+        while self.figure_widget:
+            self.figure_widget.pop().destroy()
+
     def open_popup(self, text="Hello world!"):
         top = tk.Tk()
         top.geometry("750x250")
@@ -179,8 +194,10 @@ class App(tk.Tk):
         #self.mode_frm.grid(row=1, column=2, sticky="ns")
         #self.mode_frm.pack(side=tk.RIGHT, fill='both', expand=False)
         last_row = -1
+        last_column = -1
         if self.mode_btn:
             last_row = self.mode_btn[-1].grid_info()['row']
+            last_column = self.mode_btn[-1].grid_info()['column']
         if argv:
             if len(argv) == 1:
                 self.mode_btn.append(tk.Button(self.mode_frm, text=str(text), command=lambda: func(argv[0])))
@@ -192,7 +209,8 @@ class App(tk.Tk):
                 self.mode_btn.append(tk.Button(self.mode_frm, text=str(text), command=func))
         else:
             self.mode_btn.append(tk.Button(self.mode_frm, text=str(text), command=func))
-        self.mode_btn[-1].grid(row=last_row+1, column=0, sticky="ew", padx=5, pady=5)
+        #self.mode_btn[-1].grid(row=last_row+1, column=0, sticky="ew", padx=5, pady=5)
+        self.mode_btn[-1].grid(row=int((last_column+1)/6), column=(last_column+1)%6, sticky="ew", padx=5, pady=5)
         return self.mode_btn[-1]
 
     def print_by_press(self, arg1=None, arg2=None):
@@ -263,6 +281,8 @@ class App(tk.Tk):
             return
         with open(filepath, mode="r", encoding="utf-8") as input_file:
             text = text_format.Parse(input_file.read(), MicroBumpLayout_pb2.Layouts())
+            self.app_clear()
+            self.contruct_app(len(text.MicroBumpArray))
             self.database = text
             self.load_bumps(layouts, text)
         self.title(f"Bump layout - {filepath}")
@@ -288,14 +308,15 @@ class App(tk.Tk):
             item.remove()
             del item
     def flush_fig(self):
-        self.figure.set_dpi(self.dpi)
-        self.figure_die2.set_dpi(self.dpi)
-        self.figure.canvas.draw()
-        self.figure.canvas.flush_events()
-        self.dies[0].ax.autoscale_view()
-        self.figure_die2.canvas.draw()
-        self.figure_die2.canvas.flush_events()
-        self.dies[1].ax.autoscale_view()
+        for fig in self.figures:
+            fig.set_dpi(self.dpi)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+        #width = self.frm_figure.winfo_width()
+        #height = self.frm_figure.winfo_height()
+        #self.frm_figure.config(width=width*(self.dpi/60), height=height*(self.dpi/60))
+        #for i, widget in enumerate(self.figure_widget):
+        #    widget.grid(row=int(i/2), column=i%2)
 
     def drawMicroBumps(self, target_array, drawbundles=True):
         if target_array.protomarray and target_array.ax:
@@ -310,12 +331,16 @@ class App(tk.Tk):
                 xy = (item.x*self.scaler, item.y*self.scaler)
                 size = item.size
                 type = bumpType[int(item.type)]
+                linewidth = 1
+                linestyle = 'solid'
                 if type == 'Function':
                     edgecolor = 'black'
                     facecolor = colorBoard[item.bundle%15 if drawbundles else -1]
+                    linewidth = 2
                 elif type == 'Spare':
                     edgecolor = 'orange'
                     facecolor = colorBoard[item.bundle%15 if drawbundles else -1]
+                    linewidth = 2
                 elif type == 'Vdd':
                     edgecolor = 'red'
                     facecolor = 'white'
@@ -327,9 +352,15 @@ class App(tk.Tk):
                 else:
                     edgecolor = colorBoard[-1]
                     facecolor = colorBoard[-1]
+                if item.direction == 0:
+                    linestyle = 'dashed'
+                elif item.direction == 1:
+                    linestyle = 'solid'
+                elif item.direction == 2:
+                    linestyle = 'solid'
                 ax.add_patch(plt.Circle(xy=xy, radius=size*self.scaler*180.0,
                                              edgecolor=edgecolor, facecolor=facecolor,
-                                             linewidth=1, zorder=10))
+                                             linewidth=linewidth, zorder=10, linestyle=linestyle))
                 target_array.phybumpText.append( ax.text(item.x-6, item.y+4, item.name, fontsize=10, zorder=11) )
                 x.append(float(item.x))
                 y.append(float(item.y))
@@ -595,6 +626,13 @@ class App(tk.Tk):
         self.textbox.delete('1.0', tk.END)
         self.textbox.insert(tk.END, text)
 
+class MouseEvent():
+    def __init__(self, array, func=None, argv=None):
+        self.array = array
+        self.func = func
+    def mouseclick_wrapper(self, event):
+        if event.dblclick and event.xdata:
+            self.func(event, self.array)
 
 if __name__ == "__main__":
     app = App()
